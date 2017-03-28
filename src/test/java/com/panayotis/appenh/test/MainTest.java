c58aa9797d215f2665c09320fcdc9b409ce88024
Panayotis Katsaloulis <panayotis@panayotis.com>
2017-03-28 20:15:28 +0300

    Initial import

---
+++ src/test/java/com/panayotis/appenh/test/MainTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.panayotis.appenh.test;
+
+import com.panayotis.appenh.EnhancerManager;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author teras
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        EnhancerManager.getDefault().registerAbout(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("About clicked");
+            }
+        });
+        EnhancerManager.getDefault().registerPreferences(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Preferences clicked");
+                new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            sleep(3000);
+                        } catch (InterruptedException ex) {
+                        }
+                        EnhancerManager.getDefault().requestAttention();
+                    }
+                }.start();
+            }
+        });
+        JFrame f = new JFrame();
+        f.setVisible(true);
+        f.setSize(400, 300);
+    }
+}
